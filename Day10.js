8ebeb06fbcd943cf0cfe6d25a6b68d5029578359
Prg525 <102379034+Prg525@users.noreply.github.com>
2025-08-24 21:02:17 +0530

    Day10.js
    Daily Program challenge

--- Day10.js
+++ Day10.js
@@ -1,3 +1,16 @@
+/* Problem : Group Anagrams
+You are given an array of strings strs[]. Your task is to group all the strings that are anagrams of each other. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+The goal is to return the grouped anagrams as a list of lists, where each sublist contains words that are anagrams of each other.
+
+Input :
+An array of strings strs[] consisting of lowercase English letters.
+Input: strs[] = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+Output :
+A list of lists, where each sublist contains strings that are anagrams of each other. The order of the output groups does not matter.
+Output: [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]] */
+
+
 function groupAnagrams(strs) {
     let map = new Map();
 
